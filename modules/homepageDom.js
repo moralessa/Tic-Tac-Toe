5703d998cb4c2c50d1c9804736dd9c1dcc883cd0
Samuel Morales <sam@moralessa.dev>
2022-12-07 15:51:41 -0700

    added toggles to buttons

--- modules/homepageDom.js
+++ modules/homepageDom.js
@@ -7,11 +7,14 @@ export function bootUpGame(){
     let gameStartButton = document.getElementById('start');
     let buttons = [];
     buttons.push(playerOneButton, playerTwoButton, cpuOneButton, cpuTwoButton);
+    let cpuButtons = [];
+    cpuButtons = [];
+    cpuButtons.push(cpuOneButton, cpuTwoButton);
     
     
     // Function to check if parent node already contains a selected item if it does it returns the 
     // selected item
-    function checkToggle(element){ 
+    function checkParentContainsSelected(element){ 
         let parentNode = element.parentNode;
         let selectedItem = document.querySelectorAll('.selected');
         let hasSelected;
@@ -23,16 +26,35 @@ export function bootUpGame(){
         return hasSelected;
     }
 
+    //Function to revert CPU text content back to 'CPU'
+    function revertCPU(element){
+        if(!element.classList.contains('selected')){
+            element.textContent = 'CPU';
+        }
+    }
+
     // Function to toggle active styling using checkToggle function if it has a sibling element 
     // that has already been selected the class is removed
     function toggleActive(element){
         return function(){
-            let hasSelected = checkToggle(element);
-            if(hasSelected){
-                hasSelected.classList.remove('selected');
-            } 
-            element.classList.toggle('selected');
+            let hasSelected = checkParentContainsSelected(element);
+            if(hasSelected !== element){
+                element.classList.toggle('selected');
+                hasSelected.classList.toggle('selected');
+                if(hasSelected === cpuOneButton || hasSelected === cpuTwoButton){ // conditional to check if previously selected item is a CPU
+                    revertCPU(hasSelected);
+                }
+            }
         }
+    }
+
+    //Function to toggle text on CPU buttons
+    function toggleText(element){
+        return function (){
+            if(element.textContent === 'CPU' || element.textContent === 'Impossible') element.textContent = 'Easy';
+            else if(element.textContent === 'Easy') element.textContent = 'Hard';
+            else element.textContent = 'Impossible';
+        }   
     }
 
     
@@ -41,7 +63,10 @@ export function bootUpGame(){
         button.addEventListener('click', toggleActive(button));
     })
 
-
+    //For each loop to add event listener to each CPU button
+    cpuButtons.forEach(button =>{
+        button.addEventListener('click', toggleText(button));
+    })
     
 }
 
